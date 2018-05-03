3a7ecccb3a011e29a0730b1b6d56eeaa5834baf2
Richard Paez <richardp809@gmail.com>
2018-05-02 21:05:49 -0400

    most game logic done except for remaining guesses

--- app.js
+++ app.js
@@ -5,17 +5,45 @@ const Game = {
     wordBank : ['Javascript', 'Express', 'Node', 'Sequelize', 'Mongoose', 'React', 'jQuery'],
     currentWord : null,
     score : 0,
+    triesLeft: 10,
     guessed : [],
     init: function() {
         console.log(`Welcome to the UNC Coding bootcamp CLI-Hangman Game! \nToday's topic: Languages and Libraries!`);
         this.newWord();
+        this.guess();
     },
     newWord: function() {
         if( this.wordBank.length !== 0 ){
-            this.currentWord = this.wordBank.pop();
+            this.currentWord = new Word(this.wordBank.pop());
         } else {
             //gameover
         }
+    },
+    guess: function() {
+        console.log(this.currentWord.toString());
+        inquirer.prompt({
+            name: "guess",
+            type: "input",
+            message: "Guess a letter"
+        }).then( answer => {
+            let guessedLetter = answer.guess.toLowerCase();
+            if( this.guessed.indexOf( guessedLetter ) === -1 ){
+                this.currentWord.guess(answer.guess);
+                this.guessed.push(guessedLetter);
+            } else {
+                console.log("You've already guessed that letter, try again");
+            }
+
+            if( this.currentWord.isSolved ){
+                this.score++;
+                this.guessed = [];
+                console.log(this.currentWord.toString());
+                console.log(`Awesome! let's try a new word. Score: ${this.score}`);
+                this.triesLeft = 10;
+                this.newWord();
+            }
+            this.guess();
+        });
     }
 };
 
